cc819bf11ec59910e4046404577b17d2cd7a7282
Gabriel Farias <gabriel.farias3126@gmail.com>
2022-05-07 23:08:17 -0300

    feat: use .env insted of hardcode valeus

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { HealthCheckModule } from './health-check/health-check.module';;
 import { TaskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose'
 
+import 'dotenv/config'
 @Module({
-  imports: [TaskModule, HealthCheckModule, MongooseModule.forRoot('mongodb://localhost/task')],
+  imports: [TaskModule, HealthCheckModule, MongooseModule.forRoot(process.env.MONGO_URL ?? 'mongodb://localhost/task')],
 
 })
 export class AppModule { }
